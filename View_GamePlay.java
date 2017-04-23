c85aa5c208a679b4e97d4519d66f46d4844e271d
AditPratama <aditya.psantoso@gmail.com>
2017-04-23 14:07:36 +0700

    Add files via upload

--- View_GamePlay.java
+++ View_GamePlay.java
@@ -1,5 +1,6 @@
+package View;
+
 import javax.swing.*;
-import javax.swing.text.View;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -47,12 +48,48 @@ public class View_GamePlay extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 frameMain.setVisible(false);
-                View_Shop.getFrameShop().setSize(1366,768);
-                View_Shop.getFrameShop().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                View_Shop.getPanelShop().setBackground(Color.gray);
-                View_Shop.getPanelShop().add(View_Shop.getBackToGameButton());
-                View_Shop.getFrameShop().setContentPane(new View_Shop().getPanelShop());
-                View_Shop.getFrameShop().setVisible(true);
+                if (!View_Shop.isBuild()) {
+                    //Set frame
+                    View_Shop.getFrameShop().setSize(1366, 768);
+                    View_Shop.getFrameShop().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                    //Set panel
+                    View_Shop.getPanelShop().setBackground(Color.gray);
+
+                    //Set and add Back to game button
+                    View_Shop.getPanelShop().add(View_Shop.getBackToGameButton());
+
+                    //Set and add Back player button
+                    View_Shop.getPanelShop().add(View_Shop.getPlayerButton());
+
+                    //Set and add Back joy button
+                    View_Shop.getPanelShop().add(View_Shop.getJoyButton());
+
+                    //Set and add Back sadness button
+                    View_Shop.getPanelShop().add(View_Shop.getSadnessButton());
+                    View_Shop.getSadnessButton().setEnabled(false);
+
+                    //Set and add Back anger button
+                    View_Shop.getPanelShop().add(View_Shop.getAngerButton());
+                    View_Shop.getAngerButton().setEnabled(false);
+
+                    //Set and add Back disgust button
+                    View_Shop.getPanelShop().add(View_Shop.getDisgustButton());
+                    View_Shop.getDisgustButton().setEnabled(false);
+
+                    //Set and add Back fear button
+                    View_Shop.getPanelShop().add(View_Shop.getFearButton());
+                    View_Shop.getFearButton().setEnabled(false);
+
+                    //Set frame with panel
+                    View_Shop.getFrameShop().setContentPane(new View_Shop().getPanelShop());
+
+                    //Show frame
+                    View_Shop.getFrameShop().setVisible(true);
+                    View_Shop.setBuild(true);
+                } else {
+                    View_Shop.getFrameShop().setVisible(true);
+                }
             }
         });
     }
--- View_Shop.java
+++ View_Shop.java
@@ -1,15 +1,43 @@
+package View;
+
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import MainCharacter.Player.Player;
+
+
 /**
  * Created by adit on 22/04/17.
  */
 public class View_Shop extends JFrame {
+    private static JFrame frameShop = new JFrame();
     private static JPanel panelShop = new JPanel();
     private static JButton backToGameButton = new JButton("Back to game");
-    private static JFrame frameShop = new JFrame();
+    private static JButton playerButton = new JButton("Level up Player");
+    private static JButton joyButton = new JButton("Unlocked Joy");
+    private static JButton sadnessButton = new JButton("Unlocked Sadness");
+    private static JButton angerButton = new JButton("Unlocked Anger");
+    private static JButton disgustButton = new JButton("Unlocked Disgust");
+    private static JButton fearButton = new JButton("Unlocked Fear");
+
+    private static boolean joy = true;
+    private static boolean joyUnlocked = false;
+
+    private static boolean sadness = false;
+    private static boolean sadnessUnlocked = false;
+
+    private static boolean anger = false;
+    private static boolean angerUnlocked = false;
+
+    private static boolean disgust = false;
+    private static boolean disgustUnlocked = false;
+
+    private static boolean fear = false;
+    private static boolean fearUnlocked = false;
+
+    private static boolean build = false;
 
     public static void main(String[] args) {
         new View_Shop();
@@ -39,6 +67,62 @@ public class View_Shop extends JFrame {
         return backToGameButton;
     }
 
+    public static void setAngerButton(JButton angerButton) {
+        View_Shop.angerButton = angerButton;
+    }
+
+    public static JButton getAngerButton() {
+        return angerButton;
+    }
+
+    public static void setDisgustButton(JButton disgustButton) {
+        View_Shop.disgustButton = disgustButton;
+    }
+
+    public static JButton getDisgustButton() {
+        return disgustButton;
+    }
+
+    public static void setFearButton(JButton fearButton) {
+        View_Shop.fearButton = fearButton;
+    }
+
+    public static JButton getFearButton() {
+        return fearButton;
+    }
+
+    public static void setJoyButton(JButton joyButton) {
+        View_Shop.joyButton = joyButton;
+    }
+
+    public static JButton getJoyButton() {
+        return joyButton;
+    }
+
+    public static void setSadnessButton(JButton sadnessButton) {
+        View_Shop.sadnessButton = sadnessButton;
+    }
+
+    public static JButton getSadnessButton() {
+        return sadnessButton;
+    }
+
+    public static void setPlayerButton(JButton playerButton) {
+        View_Shop.playerButton = playerButton;
+    }
+
+    public static JButton getPlayerButton() {
+        return playerButton;
+    }
+
+    public static void setBuild(boolean build) {
+        View_Shop.build = build;
+    }
+
+    public static boolean isBuild() {
+        return build;
+    }
+
     public View_Shop() {
         backToGameButton.addActionListener(new ActionListener() {
             @Override
@@ -46,6 +130,92 @@ public class View_Shop extends JFrame {
                 frameShop.setVisible(false);
                 View_GamePlay.getFrameMain().setVisible(true);
             }
+        });
+
+        playerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Hedon Lu!");
+            }
+
+        });
+
+        joyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Joy!");
+                if (!joyUnlocked) {
+                    joyButton.setText("Level up Joy");
+                    joyUnlocked = true;
+                }
+
+                sadness = true;
+                if(sadness)
+                {
+                    sadnessButton.setEnabled(true);
+                }
+            }
+
+        });
+
+        sadnessButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Sadness!");
+                if (!sadnessUnlocked) {
+                    sadnessButton.setText("Level up Sadness");
+                    sadnessUnlocked = true;
+                }
+
+                anger =  true;
+                if(anger){
+                    angerButton.setEnabled(true);
+                }
+            }
+        });
+
+        angerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Anger!");
+                if (!angerUnlocked) {
+                    angerButton.setText("Level up Anger");
+                    angerUnlocked = true;
+                }
+
+                disgust = true;
+                if(disgust){
+                    disgustButton.setEnabled(true);
+                }
+            }
+        });
+        disgustButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Disgust!");
+                if (!disgustUnlocked) {
+                    disgustButton.setText("Level up Disgust");
+                    disgustUnlocked = true;
+                }
+
+                fear = true;
+                if(fear){
+                    fearButton.setEnabled(true);
+                }
+            }
+
+        });
+
+        fearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"Fear!");
+                if (!fearUnlocked) {
+                    fearButton.setText("Level up Fear");
+                    fearUnlocked = true;
+                }
+            }
+
         });
     }
 }
