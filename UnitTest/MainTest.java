d501bb61d796f1784419fca69d685ae3ba14a91f
Stevanno Hero Leadervand <13515082@std.stei.itb.ac.id>
2017-04-25 23:44:58 +0700

    unit_test

---
+++ UnitTest/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stevannohero on 4/26/17.
+ */
+public class MainTest {
+  @Test
+  public void main() throws Exception {
+  }
+
+}
---
+++ UnitTest/maincharacter/hero/HeroTest.java
@@ -0,0 +1,63 @@
+package maincharacter.hero;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stevannohero on 4/26/17.
+ */
+public class HeroTest {
+  @Test
+  public void getThreadName() throws Exception {
+  }
+
+  @Test
+  public void getHeroThread() throws Exception {
+  }
+
+  @Test
+  public void getLevel() throws Exception {
+  }
+
+  @Test
+  public void getAttPower() throws Exception {
+  }
+
+  @Test
+  public void getThereIsEnemy() throws Exception {
+  }
+
+  @Test
+  public void setAttPower() throws Exception {
+  }
+
+  @Test
+  public void setThereIsEnemy() throws Exception {
+  }
+
+  @Test
+  public void setEnemy() throws Exception {
+  }
+
+  @Test
+  public void setThreadNull() throws Exception {
+  }
+
+  @Test
+  public void run() throws Exception {
+  }
+
+  @Test
+  public void start() throws Exception {
+  }
+
+  @Test
+  public void attack() throws Exception {
+  }
+
+  @Test
+  public void levelUp() throws Exception {
+  }
+
+}
