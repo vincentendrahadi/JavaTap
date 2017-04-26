7a67df3b4ce40b4cdbcc2a3b7238971127ef1546
Stevanno Hero Leadervand <13515082@std.stei.itb.ac.id>
2017-04-26 17:44:22 +0700

    Update StageTest.java

--- UnitTest/stage/StageTest.java
+++ UnitTest/stage/StageTest.java
@@ -1,6 +1,12 @@
-package stage;
+package UnitTest.stageTest;
 
+import enemy.Enemy;
+import enemy.RegularEnemy;
+import stage.Stage;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -9,19 +15,19 @@ import static org.junit.Assert.*;
  */
 public class StageTest {
   @Test
-  public void getCurStage() throws Exception {
-  }
-
-  @Test
-  public void setCurStage() throws Exception {
-  }
-
-  @Test
-  public void getGameStage() throws Exception {
+  public void getCurStageTest() throws Exception {
+    System.out.print("TESTING GET CURRENT STAGE... ");
+    Stage x;
+    x = new Stage("stage.txt");
+    assertTrue(x.getCurStage() == 0);
+    System.out.println("DONE!");
   }
 
   @Test
   public void getCurEnemy() throws Exception {
+    RegularEnemy e = new RegularEnemy(100,"A");
+    Stage x = new Stage("stage.txt");
+    assertEquals(e.getMaxHealth(),x.getCurEnemy().getMaxHealth());
+    assertEquals(e.getName(),x.getCurEnemy().getName());
   }
-
 }
