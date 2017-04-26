b0a4e41e446302b40cf511c816ad80d3776ca13e
Stevanno Hero Leadervand <13515082@std.stei.itb.ac.id>
2017-04-26 17:46:32 +0700

    Update WorldTest.java

--- UnitTest/world/WorldTest.java
+++ UnitTest/world/WorldTest.java
@@ -1,43 +1,37 @@
-package world;
+package UnitTest.worldTest;
 
+import maincharacter.hero.Hero;
+import maincharacter.player.Player;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import stage.Stage;
+import world.World;
+import maincharacter.hero.Hero;
+import maincharacter.player.Player;
+
+import java.util.ArrayList;
 
 /**
  * Created by stevannohero on 4/26/17.
  */
 public class WorldTest {
   @Test
-  public void getCountHero() throws Exception {
+  public void getterTest()throws Exception {
+    World w;
+    Hero h = new Hero("Joy",0,0.5);
+    Player p = new Player(":asda");
+    ArrayList<Hero> ar = new ArrayList<>();
+    Stage stages = new Stage("stage.txt");
+    w = new World(p, ar, stages);
+    assertTrue(w.getCountHero() == 0);
+    assertTrue(w.getPlayer() == p);
+    assertEquals(w.getHero(0),h);
+    w.addHero();
+    assertEquals(1,w.getCountHero());
+    w.setAllThreadToNull();
+    assertTrue(w.getHero(0) == null);
+    assertTrue(w.getPlayer() == null);
   }
-
-  @Test
-  public void getStages() throws Exception {
-  }
-
-  @Test
-  public void getPlayer() throws Exception {
-  }
-
-  @Test
-  public void getHero() throws Exception {
-  }
-
-  @Test
-  public void addHero() throws Exception {
-  }
-
-  @Test
-  public void setMonster() throws Exception {
-  }
-
-  @Test
-  public void setAllThreadToNull() throws Exception {
-  }
-
-  @Test
-  public void run() throws Exception {
-  }
-
 }
