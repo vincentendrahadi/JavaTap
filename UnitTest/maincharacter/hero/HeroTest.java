45b5bc9fb5078d9c40ca3658edb57bb3c46ea9da
Vincent Endrahadi <vincentendrahadi01@gmail.com>
2017-04-26 16:38:02 +0700

    Add files via upload

--- UnitTest/maincharacter/hero/HeroTest.java
+++ UnitTest/maincharacter/hero/HeroTest.java
@@ -1,63 +1,62 @@
-package maincharacter.hero;
+package test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.*;
+import enemy.Enemy;
+import enemy.RegularEnemy;
+import maincharacter.hero.Hero;
+import org.junit.jupiter.api.Test;
 
 /**
- * Created by stevannohero on 4/26/17.
+ * Created by Vincent Endrahadi on 4/26/17.
  */
 public class HeroTest {
   @Test
   public void getThreadName() throws Exception {
+    Hero h = new Hero("Nama", 5, 10);
+    assertEquals("Nama", h.getThreadName());
   }
-
+  
   @Test
-  public void getHeroThread() throws Exception {
+  public void getLevel() throws Exception{
+    Hero h = new Hero("asd", 5, 10);
+    assertEquals(0,h.getLevel());
+    h.levelUp();
+    assertEquals(1, h.getLevel());
   }
-
+  
   @Test
-  public void getLevel() throws Exception {
+  public void getAttPower() throws Exception{
+    Hero h = new Hero("asd", 10 ,12);
+    assertEquals(10,h.getAttPower());
+    h.levelUp();
+    assertEquals(15, h.getAttPower());
   }
-
+  
   @Test
-  public void getAttPower() throws Exception {
+  public void getThereIsEnemy() {
+    Hero h = new Hero("hero", 5, 10);
+    RegularEnemy e = new RegularEnemy(10,"adfsa");
+    assertEquals(false, h.getThereIsEnemy());
+    h.setEnemy(e);
+    assertEquals(true,h.getThereIsEnemy());
   }
-
+  
   @Test
-  public void getThereIsEnemy() throws Exception {
+  public void attackEnemy() {
+    RegularEnemy e = new RegularEnemy(100,"aaa");
+    Hero h = new Hero("AAA", 10, 10);
+    
+    h.attack(e);
+    assertEquals(90,e.getCurHealth());
   }
-
+  
   @Test
-  public void setAttPower() throws Exception {
+  public void threadHero() throws Exception {
+    RegularEnemy e = new RegularEnemy(30,"Godzilla");
+    Hero h = new Hero("Superman", 10,10);
+    h.setEnemy(e);
+    h.run();
+    assertEquals(0,e.getCurHealth());
   }
-
-  @Test
-  public void setThereIsEnemy() throws Exception {
-  }
-
-  @Test
-  public void setEnemy() throws Exception {
-  }
-
-  @Test
-  public void setThreadNull() throws Exception {
-  }
-
-  @Test
-  public void run() throws Exception {
-  }
-
-  @Test
-  public void start() throws Exception {
-  }
-
-  @Test
-  public void attack() throws Exception {
-  }
-
-  @Test
-  public void levelUp() throws Exception {
-  }
-
 }
